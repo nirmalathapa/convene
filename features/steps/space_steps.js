8b55586f33089089b6dcdb717cfda18fcc50b2fb
Zee Spencer <zspencer@users.noreply.github.com>
2022-03-06 11:20:17 -0800

    Refactor: Migrate responding to Invitations to it's own Feature file
    We noticed that we were rapidly approaching the 3+ scenarios mark for
    how People can respond to Invitations, which implied that we want an
    additional feature file _just_ for that particular use-case.
    
    We pulled that out, and in a fit of 'let's have fun with it!' started
    reframing from "system test" to "Team Avatar" and that uncovered missing
    stairs in how we interact with the test data.
    
    At present, most (if not all) of our Test Data is "seeded" outside of
    the test suite; which means that as scenarios play, they can leave the
    data in a spot that doesn't work for future scenarios.
    
    It also means that we can't leverage the affordances for differentiating
    between Spaces, Rooms and Actors etc. using their names.
    
    So now we're going to start driving out APIs for creating Space
    Memberships and Authentication Methods so our Scenarios can set their
    data up themselves.
    
    Co-authored-by: Kelly Hong <KellyAH@users.noreply.github.com>
    Co-authored-by: Neer Malathapa <nirmalathapa@users.noreply.github.com>
    Co-authored-by: Zee Spencer <zspencer@users.noreply.github.com>
    Co-authored-by: Naomi Quinones <naomiquinones@users.noreply.github.com>

--- features/steps/space_steps.js
+++ features/steps/space_steps.js
@@ -4,56 +4,85 @@ const { linkParameters } = require("../lib");
 const appUrl = require("../lib/appUrl");
 const { Api } = require("../lib/Api");
 
-Given('{a} {space}', function(_, space) {
-  return true
-})
+// @deprecated
+// @todo
+//   Find all the `Given a "..." Space` and migrate to `Given a fresh "..." Space`
+//   And then once they all pass, delete this step; and rename back to `Given a "..." Space`
+Given("{a} {space}", function (_, space) {
+  return true;
+});
 
-Given('{a} fresh {space}', function(_, space) {
-  const api = new Api(appUrl(), process.env.OPERATOR_API_KEY)
+Given("{a} fresh {space}", function (_, space) {
+  const api = new Api(appUrl(), process.env.OPERATOR_API_KEY);
 
   return api.spaces().create(space);
-})
+});
 
-
-Given('a Space with a Room', function() {
+Given("a Space with a Room", function () {
   // This space intentionally left blank... For now...
   // TODO: Create a Space for each test instead of re-using the
   //       System Test Space
-})
+});
 
-Given('{a} {space} has {a} {actor}', function(_, space, _, actor) {
-  return true
-})
+Given(
+  "{a} {space} has {a} {actor}",
+  /**
+   *
+   * @param {*} _
+   * @param {Space} space
+   * @param {*} _
+   * @param {Actor} actor
+   * @returns
+   */
+  function (_, space, _, actor) {
+    const api = new Api(appUrl(), process.env.OPERATOR_API_KEY);
+    console.log({ space, actor, api })
+    return api.authenticationMethods()
+      .findOrCreateBy({ contactMethod: 'email', contactLocation: actor.email })
+      .then((authenticationMethod) => authenticationMethod.person)
+      .then((person) => api.spaceMemberships.findOrCreateBy({ space, person }))
+  }
+);
 
-Given('the {actor} is on the {space} Dashboard', async function (actor, space) {
+Given("the {actor} is on the {space} Dashboard", async function (actor, space) {
   this.space = new SpacePage(this.driver, space);
   await this.space.visit();
 });
 
-Given('the {actor} is in the {space} and in the {room}', function (actor, space, room) {
-  this.space = new SpacePage(this.driver, space);
-  return this.space.visit()
-                   .then((spacePage) => spacePage.roomCard(room).enter())
+Given(
+  "the {actor} is in the {space} and in the {room}",
+  function (actor, space, room) {
+    this.space = new SpacePage(this.driver, space);
+    return this.space
+      .visit()
+      .then((spacePage) => spacePage.roomCard(room).enter());
+  }
+);
+
+When("a {actor} adds a {room}", function (actor, room) {
+  const { space } = linkParameters({ actor, room });
+  const page = new SpaceEditPage(this.driver, space);
+  return page.visit().then((p) => p.createRoom({ room }));
 });
 
-When('a {actor} adds a {room}', function (actor, room) {
-  const { space } = linkParameters({ actor, room })
-  const page = new SpaceEditPage(this.driver, space)
-  return page.visit().then((p) => p.createRoom({ room }))
-});
+When(
+  "the {actor} visit the {space}, {room} full URL",
+  function (actor, space, room) {
+    this.space = new SpacePage(this.driver, space);
+    room.space = space;
+    return new RoomPage(this.driver, room).visit();
+  }
+);
 
-When('the {actor} visit the {space}, {room} full URL', function (actor, space, room) {
-  this.space = new SpacePage(this.driver, space)
-  room.space = space
-  return new RoomPage(this.driver, room).visit()
-});
+Then(
+  "the {space} is available at the {string} domain",
+  function (space, string) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
 
-Then('the {space} is available at the {string} domain', function (space, string) {
+Then("there is a {space}", function (space) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
-});
-
-Then('there is a {space}', function (space) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return "pending";
 });
